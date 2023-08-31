f0b6d3762a12775f284ce874ae8abb8ef1ec5b3a
Prakash Mondal <97402824+rko0211@users.noreply.github.com>
2023-08-31 19:19:45 +0530

    add initial commit

---
+++ first.js
@@ -0,0 +1,19 @@
+// Javascript uses camel case letter style for Object property , and for object it uses FIrst letter capital
+
+f1.className = "Red text-black";
+
+console.log(f1.classList);
+
+f1.classList.remove("Red");
+
+f1.classList.add("Yellow");
+
+console.log(f1.classList);
+
+f1.classList.remove("Yellow");
+
+f1.classList.toggle("Red");
+
+f1.classList.toggle("Red");
+
+console.log(f1.classList);
